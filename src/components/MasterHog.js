0e2145b9e3d01cfc4c6d5a44912c75f10fb37f33
Erica Winne <erica.winne@powerhrg.com>
2021-07-06 11:42:42 -0400

    done

--- src/components/MasterHog.js
+++ src/components/MasterHog.js
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 import Master from "../assets/master-hog.png";
 import BabyHog from "./BabyHog";
-// import offspring from "../data.js"
+import offspring from "../data.js"
 
 function MasterHog() {
   const [eyeColor, setEyeColor] = useState("blue");
@@ -40,9 +40,13 @@ function MasterHog() {
         <img id="master-blaster" src={Master} alt="" />
       </div>
       <ul className="hoglist">
-        <BabyHog />
-        <BabyHog />
-        <BabyHog />
+        {offspring.map(hog => (
+          <BabyHog hogName={hog.name}
+           hogHobby={hog.hobby}
+           eyeColor={eyeColor}
+           />
+          ))
+          }
       </ul>
     </div>
   );
