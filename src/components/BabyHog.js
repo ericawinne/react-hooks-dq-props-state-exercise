f7d6c1ef509ebd5b030a243122763c146de5c282
Kurstyn Storms <kurstyn.storms@powerhrg.com>
2021-07-06 08:58:52 -0400

    started

--- src/components/BabyHog.js
+++ src/components/BabyHog.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import normalBaby from "../assets/unadulterated-hoglette.png";
 import SunBaby from "../assets/sun-eyes.png";
 import BlueBaby from "../assets/blue-eyes.png";
@@ -7,8 +7,10 @@ import GlowingBaby from "../assets/glowing-eyes.png";
 // is there a way we could associate eye color string values with images?
 // perhaps so we could do something along the lines of `eyeColorMapper['blue'] and get back the right image?`
 
-function BabyHog(props) {
-  function handleChangeWeight(e) {
+const BabyHog = ({ pounds = 0, newWeight }) => {
+  const [weight, setWeight] = useState(pounds)
+  const handleChangeWeight = () => {
+    setWeight(weight + 1)
     // how can we reuse this for both buttons?
     // perhaps something with e.target.name === "+"
   }
@@ -16,11 +18,11 @@ function BabyHog(props) {
   return (
     <li className="hogbabies">
       <h1>Name</h1>
-      <h3>Weight:</h3>
+      <h3>Weight:{weight}</h3>
       <h3>Hobby:</h3>
       <h4>Eye Color:</h4>
 
-      <button name="+">Increase Weight</button>
+      <button onClick = {handleChangeWeight} name="+">Increase Weight</button>
       <button name="-">Decrease Weight</button>
 
       <div className="hb-wrap">
